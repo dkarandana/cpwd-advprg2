1d8baabd8c5731123d47dded28915a0e79f8b6c2
dkarandana <dhananjayakarandana@gmail.com>
2018-01-20 13:54:44 +0530

    Add files via upload

---
+++ php/web/about-us.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>$$__TITLE__$$</title>
+	<?php require 'includes/head.php';?>
+</head>
+<body>
+	<?php include 'includes/header.php';?>
+	<main>
+		<h1>About</h1>
+	</main>
+	<?php include 'includes/footer.php';?>
+</body>
+</html>
